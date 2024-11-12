850f5826e980d0feae316a012ec1b0d209e62fbb
mirzamar <132921031+mirzamar@users.noreply.github.com>
2024-11-12 16:23:44 +0100

    Testklassen angelegt, außer getter,setter, mains und Interfaces

---
+++ src/test/java/com/example/application/models/PraktikumsbeauftragterTest.java
@@ -0,0 +1,24 @@
+package com.example.application.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PraktikumsbeauftragterTest {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
