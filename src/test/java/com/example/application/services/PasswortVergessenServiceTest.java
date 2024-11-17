29921128b46dfa132da7f65f231b36fa9e3fe064
mirzamar <132921031+mirzamar@users.noreply.github.com>
2024-11-17 21:54:12 +0100

    Test für PasswortVergessenService geschrieben, der testet ob eine Exception geworfen wird wenn die Matrikelnummer nicht existiert

---
+++ src/test/java/com/example/application/services/PasswortVergessenServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.application.services;
+
+import com.example.application.models.PasswortVergessenAnfrage;
+import com.example.application.repositories.SicherheitsantwortRepository;
+import com.example.application.repositories.StudentinRepository;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PasswortVergessenServiceTest {
+
+    //zwei Mocks:
+    @MockBean // MockBean ist dafür da um die Datenbank nicht nutzen zu muessen
+    private SicherheitsantwortRepository sicherheitsantwortRepository;
+    @MockBean
+    private StudentinRepository studentinRepository;
+
+    @Autowired
+    private PasswortVergessenService passwortVergessenService;
+
+    @Test
+    void passwortVergessenTest1() {
+        PasswortVergessenAnfrage anfrage = new PasswortVergessenAnfrage("1234567", "Meier", "Mimi", "Anna");
+
+        when(studentinRepository.findByMatrikelnummer(anfrage.getMatrikelnummer()))
+                .thenReturn(Optional.empty());
+
+        IllegalStateException thrown = assertThrows(
+                IllegalStateException.class,
+                () -> passwortVergessenService.passwortVergessen(anfrage)
+        );
+    }
+}
