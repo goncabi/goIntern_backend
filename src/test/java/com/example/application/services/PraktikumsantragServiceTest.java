354d0845c1db9219d778fbdacf1ad8c1cd287895
mirzamar <132921031+mirzamar@users.noreply.github.com>
2024-11-20 22:37:30 +0100

    Kleine Aenderungen LöschenMethodeTests in PraktikumsServiceTest Klasse

--- src/test/java/com/example/application/services/PraktikumsantragServiceTest.java
+++ src/test/java/com/example/application/services/PraktikumsantragServiceTest.java
@@ -142,11 +142,15 @@ void testAntragLoeschenErfolgreich() {
     Praktikumsantrag antrag = erzeugeGueltigenAntrag();
     antrag.setAntragsID(id);
 
-    when(praktikumsantragRepository.findById(id)).thenReturn(Optional.of(antrag));
+    //MockTeil: praktikumRepo soll gemockt werden:
+    //Die beiden Zeilen sagen dass die Datenbank umgangen werden soll und dass bei findById ein optional returnt werden soll und bei delteID soll nix passieren (weil beim test soll nicht wirlich was gelöscht werden in der datenbank)
+    when(praktikumsantragRepository.findById(id)).thenReturn(Optional.of(antrag)); // Ein Optional antrag wird erzeugt und zurück gegeben
     doNothing().when(praktikumsantragRepository).deleteById(id);
 
-    assertDoesNotThrow(() -> praktikumsantragService.antragLoeschen(id));
+    //hier wird die loschenMethode aufgerufen und der antrag mit der id wird gelöscht
+    praktikumsantragService.antragLoeschen(id);
 
+    //ähnlich wie assert. Hier wird getestet wie oft findyById aufgerufen wird und wie oft die ID gelöscht wird
     verify(praktikumsantragRepository, times(1)).findById(id);
     verify(praktikumsantragRepository, times(1)).deleteById(id);
 }
@@ -154,7 +158,7 @@ void testAntragLoeschenErfolgreich() {
 
     @Test
     void testAntragLoeschenNichtVorhanden() {
-        Long id = 999L;
+        Long id = 22L;
 
         when(praktikumsantragRepository.findById(id)).thenReturn(Optional.empty());
 
@@ -165,7 +169,7 @@ void testAntragLoeschenErfolgreich() {
                 exception.getMessage());
 
         verify(praktikumsantragRepository, times(1)).findById(id);
-        verify(praktikumsantragRepository, never()).deleteById(anyLong());
+        verify(praktikumsantragRepository, times(0)).deleteById(id);
     }
 
 
