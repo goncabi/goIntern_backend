11158ce7d6b673dec4fd8d1baec4e477e65c8e52
mirzamar <132921031+mirzamar@users.noreply.github.com>
2024-11-20 22:39:52 +0100

    imports geloescht die nicht verwendet werden

--- src/test/java/com/example/application/services/PraktikumsantragServiceTest.java
+++ src/test/java/com/example/application/services/PraktikumsantragServiceTest.java
@@ -15,17 +15,6 @@ import static org.mockito.Mockito.when;
 import java.util.Optional;
 
 
-import com.example.application.models.Praktikumsantrag;
-import com.example.application.repositories.PraktikumsantragRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
