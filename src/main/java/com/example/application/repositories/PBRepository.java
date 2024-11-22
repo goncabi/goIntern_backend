51cbef029341fc1b9f3710c1f33195121d80e42f
Merlind C. Pohl <merlind@MerlindPC.>
2024-11-22 17:48:43 +0100

    git add* git commit -m "PB-Repository hinzugefügt"

--- src/main/java/com/example/application/repositories/PBRepository.java
+++ src/main/java/com/example/application/repositories/PBRepository.java
@@ -1,9 +1,13 @@
 package com.example.application.repositories;
-
 import com.example.application.models.Praktikumsbeauftragter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+import java.util.Optional;
+
+
 @Repository
-public interface PraktikumsbeauftragterRepository extends JpaRepository<Praktikumsbeauftragter, Long> {
+public interface PBRepository extends JpaRepository<Praktikumsbeauftragter, Long> {
+    Optional<Praktikumsbeauftragter> findByUsername(String username);
 }
