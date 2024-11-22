c478e330a19bd9a59d15e7584a7b52fa027c7aba
Merlind C. Pohl <merlind@MerlindPC.>
2024-11-22 17:54:10 +0100

    git add* git commit -m "PBService hinzugefügt"

---
+++ src/main/java/com/example/application/services/PBService.java
@@ -0,0 +1,24 @@
+package com.example.application.services;
+
+import com.example.application.models.Praktikumsbeauftragter;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import com.example.application.repositories.PBRepository;
+
+@Service
+@AllArgsConstructor
+
+
+public class PBService {
+
+    private final PBRepository praktikumsbeauftragterRepository;
+
+    public void signUpUser(Praktikumsbeauftragter praktikumsbeauftragter) {
+        boolean userExists = praktikumsbeauftragterRepository.findByUsername(praktikumsbeauftragter.getUsername()).isPresent();
+        if(userExists) {
+            throw new IllegalStateException("Username existiert bereits");
+        }
+        praktikumsbeauftragterRepository.save(praktikumsbeauftragter);
+    }
+
+}
