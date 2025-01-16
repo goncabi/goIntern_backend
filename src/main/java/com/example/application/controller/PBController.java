3de4fe5c3bc697af8c3b8c80cf125edddaa84c4e
Mira.Khreis <mira@example.com>
2025-01-16 18:32:47 +0100

    Dokumentation im PBController

--- src/main/java/com/example/application/controller/PBController.java
+++ src/main/java/com/example/application/controller/PBController.java
@@ -8,6 +8,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller für die Verwaltung der Praktikumsanträge durch Praktikumsbeauftragte.
+ * <p>Diese Klasse stellt REST-API-Endpunkte bereit, um Praktikumsanträge zu genehmigen, abzulehnen oder zu übermitteln. Sie dient als Schnittstelle für die Praktikumsbeauftragten und ermöglicht die Interaktion mit dem Frontend.</p>
+ *
+ * <h2>Verfügbare Endpunkte</h2>
+ * <ul>
+ *     <li><b>POST /pb/antrag/genehmigen:</b> Genehmigt einen Praktikumsantrag</li>
+ *     <li><b>POST /pb/antrag/ablehnen/{matrikelnummer}:</b> Lehnt einen Antrag ab und fügt einen Kommentar hinzu.</li>
+ *     <li><b>POST /pb/antrag/übermitteln:</b> Übermittelt einen Praktikumsantrag.</li>
+ *</ul>
+ *
+ * <h2>Fehlermeldung</h2>
+ * <ul>
+ *     <li><b>400 BAD REQUEST:</b> Fehlerhafte Eingaben oder ungültige Parameter.</li>
+ *     <li><b>500 INTERNAL SERVER ERROR:</b> Ein unerwarteter Fehler ist aufgetreten.</li>
+ * </ul>
+ *
+ * <h2> Abhängigkeiten:</h2>
+ * <ul>
+ *     <li>{@link PBService}: Geschäftslogik für die Verarbeitung von Praktikumsanträgen.</li>
+ * </ul>
+ */
+
 @RestController
 @RequestMapping("/pb")
 @AllArgsConstructor
