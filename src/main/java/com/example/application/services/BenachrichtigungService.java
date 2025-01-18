13fdd14247a8788cfbb2587fe135703e2a5eec97
Mira.Khreis <mira@example.com>
2025-01-19 00:43:36 +0100

    BenachrichtigungService Dokumentation

--- src/main/java/com/example/application/services/BenachrichtigungService.java
+++ src/main/java/com/example/application/services/BenachrichtigungService.java
@@ -6,15 +6,32 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
+/**
+ * Service Klasse für die Verwaltung von Benachrichtigungen.
+ *
+ * Die Klasse bietet Funktionen, um Benachrichtigungen für Nutzer zu lesen und zu löschen.
+ */
+
 @Service
 @AllArgsConstructor
 public class BenachrichtigungService {
 
     private final BenachrichtigungRepository benachrichtigungRepository;
 
+    /**
+     * Liest alle benachrichtigungen für einen bestimmten Empfänger.
+     * @param empfaenger_in Die Matrikelnummer des Empfängers.
+     * @return eine Liste von Benachrichtigungen, sortiert nach datum
+     */
+
     public List<Benachrichtigung> alleLesen(String empfaenger_in){
         return benachrichtigungRepository.findByEmpfaengerOrderByDatum(empfaenger_in);
     }
+
+    /**
+     * löscht alle Benachrichtigungen für einen bestimmten Empfänger.
+     * @param empfaenger_in die Matrikelnummer des Empfängers
+     */
 
     public void nachrichtenLoeschen(String empfaenger_in){
         List<Benachrichtigung> nachrichtenListe = benachrichtigungRepository.findByEmpfaengerOrderByDatum(empfaenger_in);
