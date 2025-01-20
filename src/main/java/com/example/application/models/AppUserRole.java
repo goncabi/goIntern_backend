71500b4aed9b709d610234b6766d60dac05e45b6
Angela <a.barzaeva@gmail.com>
2025-01-20 21:17:13 +0100

    Dokumentation für AppUserRole

--- src/main/java/com/example/application/models/AppUserRole.java
+++ src/main/java/com/example/application/models/AppUserRole.java
@@ -1,5 +1,10 @@
 package com.example.application.models;
 
+/**
+ * Enum, der die verschiedenen Benutzerrollen innerhalb der Anwendung definiert.
+ *
+ * Es gibt zwei Rollen: STUDENTIN und PRAKTIKUMSBEAUFTRAGTER.
+ */
 public enum AppUserRole {
     STUDENTIN, PRAKTIKUMSBEAUFTRAGTER
 }
